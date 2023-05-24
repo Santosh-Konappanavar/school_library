606b9d5564563ca690b1b0b77993cb2462eff3ab
Santosh-Konappanavar <siddukundaragi20@gmail.com>
2023-05-24 18:02:11 +0530

    ADD:class for school library

---
+++ Teacher.rb
@@ -0,0 +1,11 @@
+require_relative 'Person'
+class Teacher < Person
+    def initialize(age, parent_permission = true, name = "Unknown", specialization)
+      super(age, parent_permission, name)
+      @specialization = specialization
+    end
+  
+    def can_use_services?
+      true
+    end
+  end
