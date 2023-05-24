a16e2bce8126fef0065a4d339b064642211f9059
Santosh-Konappanavar <siddukundaragi20@gmail.com>
2023-05-24 18:12:13 +0530

    FIX:linters error

--- Person.rb
+++ Person.rb
@@ -1,40 +1,40 @@
 class Person
-    attr_reader :id
-  
-    def initialize(age, parent_permission = true, name = "Unknown")
-      @id = generate_id
-      @name = name
-      @age = age
-      @parent_permission = parent_permission
-    end
-  
-    def name
-      @name
-    end
-  
-    def age
-      @age
-    end
-  
-    def name=(new_name)
-      @name = new_name
-    end
-  
-    def age=(new_age)
-      @age = new_age
-    end
-  
-    def can_use_services?
-      of_age? || @parent_permission
-    end
-  
-    private
-  
-    def of_age?
-      @age >= 18
-    end
-  
-    def generate_id
-      rand(1..1000)
-    end
+  attr_reader :id
+
+  def initialize(age, parent_permission = true, name = "Unknown")
+    @id = generate_id
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
   end
+
+  def name
+    @name
+  end
+
+  def age
+    @age
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def age=(new_age)
+    @age = new_age
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+
+  def generate_id
+    rand(1..1000)
+  end
+end
--- Student.rb
+++ Student.rb
@@ -1,12 +1,11 @@
 require_relative 'Person'
 class Student < Person
-    def initialize(age, parent_permission = true, name = "Unknown", classroom)
-      super(age, parent_permission, name)
-      @classroom = classroom
-    end
-  
-    def play_hooky
-      "¯\\(ツ)/¯"
-    end
+  def initialize(age, parent_permission = true, name = "Unknown", classroom)
+    super(age, parent_permission, name)
+    @classroom = classroom
   end
-  
+
+  def play_hooky
+    "¯\\(ツ)/¯"
+  end
+end
